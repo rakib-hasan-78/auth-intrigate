1c042dbd91e5f75d120cf760bc1887f8a4af5bbf
rakib hasan <rakibwebdev78@gmail.com>
2026-05-24 15:57:39 +0600

    footer added...

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        
+        <footer class="$$footer $$sm:footer-horizontal $$footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by HSB INC.</p>
+            </aside>
+        </footer>
+    );
+};
+
+export default Footer;
