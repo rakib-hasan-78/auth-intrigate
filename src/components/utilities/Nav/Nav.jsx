a0b864bc762b835241a9cf365a4938f09cdfc7be
rakib hasan <rakibwebdev78@gmail.com>
2026-06-09 20:06:30 +0600

    login components based on logic added...

--- src/components/utilities/Nav/Nav.jsx
+++ src/components/utilities/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router';
 import { useAuth } from '../../TestContest/TestContext';
 
 const Nav = () => {
@@ -31,10 +31,41 @@ const Nav = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">{loginUser ? 'logout':'login'}</a>
+    {
+      loginUser ?
+      <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+      </div>
+      :
+      <Link to={'/login'} >
+       login 
+      </Link>
+
+    }
   </div>
+
+
 </div>
     );
 };
 
 export default Nav;
+
